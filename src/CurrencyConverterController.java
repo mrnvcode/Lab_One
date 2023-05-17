f814c08636af3a5f4b04fc4d484441278c126fea
mrnvcode <200532226@student.georgianc.on.ca>
2023-05-17 05:02:50 -0400

    Updated the Project by adding notes for better understanding of the code.

--- src/CurrencyConverterController.java
+++ src/CurrencyConverterController.java
@@ -16,6 +16,7 @@ public class CurrencyConverterController {
     @FXML
     private TextField convertedAmountTextField;
 
+    // Conversion rates from USD to other currencies
     private static final double USD_TO_EGP = 30.85;
     private static final double USD_TO_YEN = 136.99;
     private static final double USD_TO_INR = 82.38;
@@ -24,32 +25,40 @@ public class CurrencyConverterController {
 
     @FXML
     private void initialize() {
+        // Populate the source and target currency combo boxes
         sourceCurrencyComboBox.getItems().addAll("USD", "EGP", "YEN", "INR", "GBP", "EURO");
         targetCurrencyComboBox.getItems().addAll("USD", "EGP", "YEN", "INR", "GBP", "EURO");
     }
 
     @FXML
     private void handleConvertButtonClicked() {
+        // Get the selected source and target currencies
         String sourceCurrency = sourceCurrencyComboBox.getValue();
         String targetCurrency = targetCurrencyComboBox.getValue();
 
         if (sourceCurrency != null && targetCurrency != null) {
+            // Parse the amount to be converted
             double amount = Double.parseDouble(amountTextField.getText());
 
             double convertedAmount;
             if (sourceCurrency.equals("USD")) {
+                // Conversion from USD to non-USD currency
                 convertedAmount = convertUSDtoNonUSD(amount, targetCurrency);
             } else if (targetCurrency.equals("USD")) {
+                // Conversion from non-USD to USD currency
                 convertedAmount = convertNonUSDToUSD(amount, sourceCurrency);
             } else {
+                // Conversion between two non-USD currencies
                 double amountInUSD = convertNonUSDToUSD(amount, sourceCurrency);
                 convertedAmount = convertUSDtoNonUSD(amountInUSD, targetCurrency);
             }
 
+            // Set the converted amount in the text field
             convertedAmountTextField.setText(String.valueOf(convertedAmount));
         }
     }
 
+    // Convert amount from USD to the specified non-USD currency
     private double convertUSDtoNonUSD(double amount, String targetCurrency) {
         switch (targetCurrency) {
             case "EGP":
@@ -67,6 +76,7 @@ public class CurrencyConverterController {
         }
     }
 
+    // Convert amount from the specified non-USD currency to USD
     private double convertNonUSDToUSD(double amount, String sourceCurrency) {
         switch (sourceCurrency) {
             case "EGP":
--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,27 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Load the CurrencyConverter.fxml file using FXMLLoader
         FXMLLoader loader = new FXMLLoader(getClass().getResource("CurrencyConverter.fxml"));
         AnchorPane root = loader.load();
-        loader.getController();
 
+        // Get the controller instance from the FXMLLoader (not used in the original code)
+
+        // Create a new scene with the root node
         Scene scene = new Scene(root);
+
+        // Set the scene for the primary stage
         primaryStage.setScene(scene);
+
+        // Set the title of the stage
         primaryStage.setTitle("Currency Converter");
+
+        // Show the primary stage
         primaryStage.show();
     }
 
     public static void main(String[] args) {
+        // Launch the JavaFX application
         launch(args);
     }
 }
